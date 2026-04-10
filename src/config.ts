d94ecb580c7448fbcaac46f7386ff9a14dfb8562
Anmol Choudhary <3anmol3@gmail.com>
2026-04-10 14:39:07 +0530

    feat: site ownership

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-export const AD_CLIENT = 'ca-pub-XXXXXXXXXXXXXXXX';
+export const AD_CLIENT = 'ca-pub-8402413061735154';
 export const SITE_NAME = 'Ultimate Tic-Tac-Toe';
 export const SITE_URL = 'https://tic-tac-toe-9x9.vercel.app';
